e3567b26446b90ea4140472fe65ae85064b2f043
Yaser Eftekhari <yaser.eftekhari@gmail.com>
2021-07-11 21:24:57 -0400

    filling category screen with some items in another file

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'category_item.dart';
+
+import 'dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
@@ -6,10 +9,16 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      children: [],
+      children: DUMMY_CATEGORIES
+          .map((category) => CategoryItem(
+                color: category.color,
+                title: category.title,
+              ))
+          .toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
-        childAspectRatio: 3/2, // This indicates what is the height vs width ratio of the items in the grid
+        childAspectRatio: 3 /
+            2, // This indicates what is the height vs width ratio of the items in the grid
         crossAxisSpacing: 20,
         mainAxisSpacing: 20,
       ),
