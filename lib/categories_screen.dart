0be892b7df957a4c5bb9e2d6b0b8a389e03d45d1
Yaser Eftekhari <yaser.eftekhari@gmail.com>
2021-07-11 21:35:09 -0400

    adding scaffold to category screen; some code clean up on main

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -8,19 +8,22 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES
-          .map((category) => CategoryItem(
-                color: category.color,
-                title: category.title,
-              ))
-          .toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 /
-            2, // This indicates what is the height vs width ratio of the items in the grid
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(title: const Text("Meal Categories"),),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map((category) => CategoryItem(
+                  color: category.color,
+                  title: category.title,
+                ))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 /
+              2, // This indicates what is the height vs width ratio of the items in the grid
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
